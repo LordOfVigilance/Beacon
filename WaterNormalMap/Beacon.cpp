907577ab471ab55fc00c0972a832446933582f42
Conrad Barrington <cbarring@uwo.ca>
2016-03-31 16:16:58 -0400

    Added a Beacon class and some more models

---
+++ WaterNormalMap/Beacon.cpp
@@ -0,0 +1,29 @@
+#include "Beacon.h"
+
+
+
+Beacon::Beacon(): model(Model("Models/Monkey.obj"))
+{
+	position = glm::vec3();
+	rotation = glm::vec2();
+	model.translate(position);
+	model.rotate(rotation.x, glm::vec3(0.0f, 1.0f, 0.0f));
+}
+
+Beacon::Beacon(glm::vec3 positionIn, glm::vec2 rotationIn, Model modelIn): model(modelIn)
+{
+	position = positionIn;
+	rotation = rotationIn;
+	model.translate(position);
+	model.rotate(rotation.x, glm::vec3(0.0f, 1.0f, 0.0f));
+}
+
+
+Beacon::~Beacon()
+{
+}
+
+glm::vec3 Beacon::getPosition()
+{
+	return position;
+}
---
+++ WaterNormalMap/Beacon.h
@@ -0,0 +1,17 @@
+#include "glm/glm.hpp"
+#include "Model.h"
+
+#pragma once
+class Beacon
+{
+public:
+	Beacon();
+	Beacon(glm::vec3 positionIn, glm::vec2 rotationIn, Model modelIn);
+	~Beacon();
+	glm::vec3 getPosition();
+private:
+	glm::vec3 position;
+	glm::vec2 rotation;
+	Model model;
+};
+
